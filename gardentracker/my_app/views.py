6236444414b91ced31edb5658b4d285e8ab24963
guyera <thegraygrimm@graymansurvivaltech.com>
2025-09-08 21:46:46 -0400

    created the plant index page

--- gardentracker/my_app/views.py
+++ gardentracker/my_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Plant:
+    def __init__(self, name, variety, category):
+        self.name = name
+        self.variety = variety
+        self.category = category
+
+plants = [
+    Plant('Green Bean', 'Contender', 'Vegeteable',),
+    Plant('Summer Squash', 'Chifon squash', 'Vegetable'),
+    Plant('Rosemary', 'Bush', 'Herb')
+]
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Welcome to the Homesteaders Garden Journal</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def plant_index(request):
+    return render(request, 'plants/index.html', {'plants': plants})
