cc6211a825c5936586e418f882d6a8a1aebc28e5
guyera <thegraygrimm@graymansurvivaltech.com>
2025-09-12 16:56:25 -0400

    debugging

--- my_app/models.py
+++ my_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 # Create your models here.
 class Plant(models.Model):
     name = models.CharField(max_length=100)
@@ -10,6 +11,15 @@ class Plant(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('plant-detail', kwargs={'plant_id': self.id})
+        return reverse("plant-detail", kwargs={"plant_id": self.id})
+
+
+class Watering(models.Model):
+    date = models.DateField("Watering Date")
+
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}"
--- my_app/views.py
+++ my_app/views.py
@@ -1,31 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Plant
+from .forms import WateringForm
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def plant_index(request):
     plants = Plant.objects.all()
-    return render(request, 'plants/index.html', {'plants': plants})
+    return render(request, "plants/index.html", {"plants": plants})
+
 
 def plant_detail(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
-    print(plant)
-    return render(request, 'plants/detail.html', {'plant': plant})
+    watering_form = WateringForm()
+    return render(
+        request, "plants/detail.html", {"plant": plant, "watering_form": watering_form}
+    )
+
 
 class PlantCreate(CreateView):
     model = Plant
-    fields = '__all__'
-    
+    fields = "__all__"
+
+
 class PlantUpdate(UpdateView):
     model = Plant
-    fields = '__all__'
-    
+    fields = "__all__"
+
+
 class PlantDelete(DeleteView):
     model = Plant
-    success_url = '/plants/'
+    success_url = "/plants/"
+
+
+def add_watering(request, plant_id):
+    form = WateringForm(request.POST)
+    if form.is_valid():
+        new_watering = form.save(commit=False)
+        new_watering.plant_id = plant_id
+        new_watering.save()
+    return redirect("plant-detail", plant_id=plant_id)
